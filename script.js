c2ec69970de2c02793bddf16bbfd4d0f2966825f
rafr6 <raffr95@gmail.com>
2025-08-04 18:56:34 +0600

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-window.onload = function () {
-  document.getElementById("download").addEventListener("click", function () {
-    const card = document.getElementById("card");
-    html2pdf().from(card).save("romjan_id_card.pdf");
-  });
-};
